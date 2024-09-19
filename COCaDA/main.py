4d08c3202400e0f37f6824d531ab2b9c5fd22de9
rplemos <rafael_plemos@hotmail.com>
2024-09-19 17:40:23 -0300

    Refactored main

--- COCaDA/main.py
+++ COCaDA/main.py
@@ -24,15 +24,13 @@ def main():
     and runs the appropriate processing function (single-core or multi-core) on the input files.
     It also manages core affinity, output folder creation, and timing of the entire process.
     """
-
     global_time_start = timer()
     file_list, core, mode, cnum, output = argparser.cl_parse()
-    
-    if cnum: # specific core to run
-        p = Process(os.getpid())
-        p.cpu_affinity([int(cnum)])
+
+    if cnum:  # Set specific core affinity
+        Process(os.getpid()).cpu_affinity([int(cnum)])
         print(f"Running on core {cnum}")
-        
+    
     if output:
         output_folder = "./outputs/"
         if not os.path.exists(output_folder):
@@ -40,85 +38,56 @@ def main():
     else:
         output_folder = None
 
-        
-    if mode == "Single":
-        single(file_list, output, output_folder)
-    elif mode == "Multi":
-        multi(file_list, output, output_folder, core)
+    process_func = single if mode == "Single" else multi
+    process_func(file_list, output_folder, core)
 
-    print(f"Total time elapsed: {timer() - global_time_start}\n")
+    print(f"Total time elapsed: {f"{(timer() - global_time_start):.3f}s"}\n")
 
 
-def single(file_list, output, output_folder):
+def single(file_list, output_folder, core=None):
     """
     Processes a list of files in single-core mode.
 
     Args:
         file_list (list): List of file paths to process.
-        output (bool): Whether to save the results to a file.
-        output_folder (str): The directory where output files will be saved.
+        output_folder (str): The directory where output files will be saved (or None if no output).
+        core: Unused, kept for compatibility with multi-core.
 
     This function processes each file in the list sequentially, detects contacts, and outputs
     the results to the console or to a file, depending on the 'output' flag.
     """
-    
     for file in file_list:
         try:
             result = process_file(file)
-            if result:
-                protein, contacts_list, process_time = result
-                if output:
-                    with open(f"{output_folder}/{protein.id}_contacts.txt", "w") as f:
-                        print(f"ID: {protein.id} | Size: {protein.true_count():<7} | Contacts: {len(contacts_list):<7} | Time: {process_time:.4f}")
-                        f.write(f"ID: {protein.id} | Size: {protein.true_count():<7} | Contacts: {len(contacts_list):<7} | Time: {process_time:.4f}\n")
-                        f.write(contacts.show_contacts(contacts_list))
-                else:
-                    print(f"ID: {protein.id} | Size: {protein.true_count():<7} | Contacts: {len(contacts_list):<7} | Time: {process_time:.4f}")
-                    
+            process_result(result, output_folder)
         except Exception as e:
             print(f"Error: {e}")
-            
-            
-def multi(file_list, output, output_folder, core):
+
+
+def multi(file_list, output_folder, core):
     """
     Processes a list of files in multi-core mode using parallel processing.
 
     Args:
         file_list (list): List of file paths to process.
-        output (bool): Whether to save the results to a file.
-        output_folder (str): The directory where output files will be saved.
+        output_folder (str): The directory where output files will be saved (or None if no output).
         core (int): The number of cores to use for parallel processing. If set to 0, all available cores are used.
 
     This function processes the files in the list using a process pool with the specified number of cores.
-    It handles the results in parallel and outputs them to the console or a file, depending on the 'output' flag.
     """
-    
     core = cpu_count() if core == 0 else core
     print(f"Starting processing with {core} cores") 
     
     with ProcessPoolExecutor(max_workers=core) as executor:
         futures = {executor.submit(process_file, file): file for file in file_list}
-        
         for future in as_completed(futures):
             try:
-                result = future.result()
-                if result:
-                    protein, contacts_list, process_time = result
-                    if output:
-                        with open(f"{output_folder}/{protein.id}_contacts.txt", "w") as f:
-                            print(f"ID: {protein.id} | Size: {protein.true_count():<7} | Contacts: {len(contacts_list):<7} | Time: {process_time:.4f}")
-                            f.write(f"ID: {protein.id} | Size: {protein.true_count():<7} | Contacts: {len(contacts_list):<7} | Time: {process_time:.4f}\n")
-                            f.write(contacts.show_contacts(contacts_list))
-                    else:
-                        print(f"ID: {protein.id} | Size: {protein.true_count():<7} | Contacts: {len(contacts_list):<7} | Time: {process_time:.4f}")
-                        
+                process_result(future.result(), output_folder)
             except Exception as e:
                 print(f"Error: {e}")
-                
             finally:
-                del futures[future] # cleans memory to avoid bloating
-           
-                
+                del futures[future]  # Clean memory
+
 def process_file(file_path):
     """
     Processes a single file for contact detection.
@@ -133,24 +102,43 @@ def process_file(file_path):
     This function parses the PDB or mmCIF file, detects contacts, and returns the results. 
     If an error occurs during processing, it logs the error and returns None.
     """
-    
     start_time = timer()
-    
+
     try:
         parsed_data = parser.parse_pdb(file_path) if file_path.endswith(".pdb") else parser.parse_cif(file_path)
             
-        if parsed_data.true_count() > 25000: # skips very big proteins (customizable)
+        if parsed_data.true_count() > 25000:  # Skip very large proteins (customizable)
             print(f"Skipping ID '{parsed_data.id}'. Size: {parsed_data.true_count()} residues")
             return None
 
         contacts_list = contacts.contact_detection(parsed_data)
-        
         process_time = timer() - start_time
         return parsed_data, contacts_list, process_time
-    
+
     except Exception as e:
         print(f"Error processing {file_path}: {e}")
         return None
+
+
+def process_result(result, output_folder):
+    """
+    Handles the result of processing a file.
+
+    Args:
+        result (tuple): A tuple containing the processed Protein object, contacts list, and processing time.
+        output_folder (str): The directory where output files will be saved.
+    """
+    if result:
+        protein, contacts_list, process_time = result
+        output_data = f"ID: {protein.id} | Size: {protein.true_count():<7} | Contacts: {len(contacts_list):<7} | Time: {process_time:.3f}s"
+        
+        if output_folder:
+            with open(f"{output_folder}/{protein.id}_contacts.txt", "w") as f:
+                print(output_data)
+                f.write(output_data + "\n")
+                f.write(contacts.show_contacts(contacts_list))
+        else:
+            print(output_data)
 
 
 if __name__ == "__main__":
